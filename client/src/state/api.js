0633c8c29014e1bdee8b4e2d024ff2a247823841
Teddy A. DeLeon (The Lion) <tdeleon@ai-codelabs.com>
2023-04-08 17:51:24 -0700

    Update api.js

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "https://aichat-1oeo.onrender.com" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "main",
   tagTypes: [],
   endpoints: (build) => ({
